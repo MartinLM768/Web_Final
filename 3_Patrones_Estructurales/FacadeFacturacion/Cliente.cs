4bd5e62ec9024d030232bdb85737c18245315bae
sio574 <mallinlooool@gmail.com>
2025-05-14 21:47:16 -0500

    punto 2
    Utiliza el patrón Fachada (Facade) para simplificar el acceso al módulo de facturación que involucra múltiples subsistemas.

---
+++ 3_Patrones_Estructurales/FacadeFacturacion/Cliente.cs
@@ -0,0 +1,10 @@
+namespace FacadeFacturacion
+{
+    public class Cliente
+    {
+        public void ObtenerDatosCliente()
+        {
+            Console.WriteLine("Cliente verificado.");
+        }
+    }
+}
